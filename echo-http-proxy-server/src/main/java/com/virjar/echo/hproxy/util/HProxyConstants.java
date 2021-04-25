c798255b42bdb1077c724f0936362df764f9d128
virjar <virjar@virjar.com>
2021-04-25 11:27:12 +0800

    update

--- echo-http-proxy-server/src/main/java/com/virjar/echo/hproxy/util/HProxyConstants.java
+++ echo-http-proxy-server/src/main/java/com/virjar/echo/hproxy/util/HProxyConstants.java
@@ -6,7 +6,5 @@ import io.netty.util.AttributeKey;
 
 public class HProxyConstants {
     public static AttributeKey<ProxyNode> BIND_PROXY_NODE = AttributeKey.newInstance("bind_proxy_node");
-    public static final AttributeKey<DefaultChannelPromise> connectReadyPromiseKey =
-            AttributeKey.newInstance("connect-ready-promise");
     public static AttributeKey<String> ECHO_TRACE_ID = AttributeKey.newInstance("echo_trace_id");
 }

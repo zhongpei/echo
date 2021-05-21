b3cbcfe57a04a92020537a14e61a293671efb067
liguobao <codelover@qq.com>
2021-05-21 22:53:36 +0800

    关闭NAT连接前断开真实连接

--- echo-proxy-lib/src/main/java/com/virjar/echo/nat/client/EchoClient.java
+++ echo-proxy-lib/src/main/java/com/virjar/echo/nat/client/EchoClient.java
@@ -226,12 +226,8 @@ public class EchoClient {
         natChannel.writeAndFlush(proxyMessage);
         natChannel.close();
         this.isConnecting = false;
-        for(Long seq : this.allRealServerChannels.keySet()){
-            Channel readChannel = this.allRealServerChannels.get(seq);
-            if(readChannel!=null) {
-                readChannel.close();
-                this.allRealServerChannels.remove(seq);
-            }
+        for (Long seq : this.allRealServerChannels.keySet()) {
+            this.realServerDisconnect(seq);
         }
         EchoLogger.getLogger().info("closeNatChannel finish");
     }
